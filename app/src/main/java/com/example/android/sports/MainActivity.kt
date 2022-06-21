52aaa9c33c68c24fe857e21e32b660462960f1e2
Maxmarra <5919669@gmail.com>
2022-06-21 09:38:36 +0300

    starter_cleaned

--- app/src/main/java/com/example/android/sports/MainActivity.kt
+++ app/src/main/java/com/example/android/sports/MainActivity.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (c) 2021 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.sports
 
 import android.os.Bundle
@@ -38,7 +22,9 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
         val navController = navHostFragment.navController
+
         appBarConfiguration = AppBarConfiguration(navController.graph)
+
         binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 
--- app/src/main/java/com/example/android/sports/NewsDetailsFragment.kt
+++ app/src/main/java/com/example/android/sports/NewsDetailsFragment.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (c) 2021 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.sports
 
 import android.os.Bundle
@@ -25,9 +9,6 @@ import androidx.fragment.app.activityViewModels
 import coil.load
 import com.example.android.sports.databinding.FragmentSportsNewsBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
 class NewsDetailsFragment : Fragment() {
 
     private val sportsViewModel: SportsViewModel by activityViewModels()
@@ -41,6 +22,7 @@ class NewsDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         val binding = FragmentSportsNewsBinding.bind(view)
 
         // Attach an observer on the currentSport to update the UI automatically when the data
